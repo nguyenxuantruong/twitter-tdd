1059529008d3b89d14f2022deeea35fdecba71c3
Truong Nguyen <truong.nguyenxuan@asnet.com.vn>
2015-07-10 17:14:53 +0700

    inplement signup and login and finish testing for the features

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  	if @current_user
+      redirect_to @current_user
+    end
+  end
+
+  def create
+  	user = User.find_by_email(params[:session][:email].downcase)
+  	if user && user.authenticate(params[:session][:password])
+      flash[:success] = 'Logged in !!!'
+      session[:user_id] = user.id
+      redirect_to user
+  	else
+  		flash.now[:danger] = 'Invalid email/password combination'
+  		render 'new'
+  	end
+  end
+
+  def destroy
+  	session[:user_id] = nil
+    flash[:success] = "Logged out !!!"
+    redirect_to root_url
+  end
+end
---
+++ spec/features/users_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "User manager", :type => :feature do
+	background do
+		@user_attributes = attributes_for(:user)
+	end
+
+	scenario "sign up - create new user" do
+		visit root_path
+		click_link 'Sign up now!'
+		expect(page).to have_text("New User")
+		find('#user_email').set(@user_attributes[:email])
+		find('#user_password').set(@user_attributes[:password])
+		find('#user_password_confirmation').set(@user_attributes[:password_confirmation])
+		click_button 'Submit'
+		expect(page).to have_text(@user_attributes[:email])
+	end
+
+	scenario "Login" do
+		user = User.create(@user_attributes)
+		visit root_path
+		click_link "Log in"
+		expect(page).to have_text("Log in")
+		find('#session_email').set(@user_attributes[:email])
+		find('#session_password').set(@user_attributes[:password])
+		click_button 'Log in'
+		expect(page).to have_text("Logged in !!!")
+		expect(page).to have_text(@user_attributes[:email])
+	end
+end
