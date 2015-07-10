b402f95fb88c754e228bb0eb37810ede2b4db7fd
Truong Nguyen <truong.nguyenxuan@asnet.com.vn>
2015-07-10 15:06:36 +0700

    finish testing for user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-	attr_accessor :email, :password, :password_confirmation
-
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   has_secure_password
